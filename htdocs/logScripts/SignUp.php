f6ff375e7f2322fc4fd614cec801b62a16cccd25
Feliciameyer1 <51824459+Feliciameyer1@users.noreply.github.com>
2019-09-09 19:20:15 -0400

    added some php code
    I added the connection string and have started validating input on the server-side

--- htdocs/logScripts/SignUp.php
+++ htdocs/logScripts/SignUp.php
@@ -1,1 +1,23 @@
 <?php
+if(isset($_POST['signUp-submit'])){
+    //takes input from form into variable
+    $username = $_POST['Email'];
+    $password = $_POST['password'];
+    $fname=$_POST['Fname'];
+    $Lname=$_POST['Lname'];
+    $Address=$_POST['Address'];
+    $city = $_POST['City'];
+    $state = $_POST['State'];
+    $zip = $_POST['Zip'];
+    $role = $_POST['Role'];
+    $connection = new MongoClient("mongodb+srv://vetchecksiteuser:p@ssw0rd1!@vetcheck-cdi31.mongodb.net/test?retryWrites=true&w=majority");
+    $db = $client->test;
+    //checks for empty fields
+    if(empty($username) || empty($password) || empty($fname) || empty($Lname) || empty($Address) || empty($city) || empty($state) || empty($zip) || empty($role)){
+    //returns the user back to the signup page with any of their filled in feilds in the url
+        header("location:../SignUp.html?error=emptyFields&uid=".$username."&fname=".$fname."&lname=".$Lname."&address=".$Address."&city=".$city."&state=".$state);
+        // stops the script from running
+        exit();
+        
+    }
+}
