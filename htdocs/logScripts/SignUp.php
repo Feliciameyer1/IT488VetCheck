109d89b5204c9464de9ff74e28450a6ca3a2c5b6
Feliciameyer1 <51824459+Feliciameyer1@users.noreply.github.com>
2019-09-10 18:38:26 -0400

    finished validation
    finished the validation and added the code to create the user.

--- htdocs/logScripts/SignUp.php
+++ htdocs/logScripts/SignUp.php
@@ -10,8 +10,17 @@ if(isset($_POST['signUp-submit'])){
     $state = $_POST['State'];
     $zip = $_POST['Zip'];
     $role = $_POST['Role'];
-    $connection = new MongoClient("mongodb+srv://vetchecksiteuser:p@ssw0rd1!@vetcheck-cdi31.mongodb.net/test?retryWrites=true&w=majority");
-    $db = $client->test;
+    // creates a variable for validation of password complexity against a regular expression
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $number    = preg_match('@[0-9]@', $password);
+    $specialChars = preg_match('@[^\w]@', $password);
+    //creates a connection with the database
+    $client = new MongoDB\Client("mongodb+srv://vetchecksiteuser:p@ssw0rd1!@vetcheck-cdi31.mongodb.net/test?retryWrites=true&w=majority");
+  // specifys the Mongodb collection we will be accessing
+    $collection= $client->VetCheck;    
+   
+    
     //checks for empty fields
     if(empty($username) || empty($password) || empty($fname) || empty($Lname) || empty($Address) || empty($city) || empty($state) || empty($zip) || empty($role)){
     //returns the user back to the signup page with any of their filled in feilds in the url
@@ -19,5 +28,43 @@ if(isset($_POST['signUp-submit'])){
         // stops the script from running
         exit();
         
+    }
+    // Checks for a valid email address entered
+    else if(!filter_var($username, FILTER_VALIDATE_EMAIL)){
+        header("location:../SignUp.html?error=InvalidEmail&fname=".$fname."&lname=".$Lname."&address=".$Address."&city=".$city."&state=".$state);
+        // stops the script from running
+        exit();
+        
+    }
+    // Validates the password complexity. The password must be atleast eight charectors long with one uppercase letter, one lowercase letter, one number, and one special charector
+    else if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8){
+        header("location:../SignUp.html?error=WeakPassword&uid=".$username."&fname=".$fname."&lname=".$Lname."&address=".$Address."&city=".$city."&state=".$state);
+        // stops the script from running
+        exit();
+        
+    }
+    else{
+        $existingAccountcheck=$collection->findone(['email' => $username]);
+        if($existingAccountcheck>0){
+            //creates a new user if no results are returned
+            $insertNewUser=$collection->insertone([
+                'firstname' => $fname,
+                'lastname' => $Lname,
+                'email' => $username,
+                'role' => $role,
+                'address'=> $Address,
+                'city' => $city,
+                'state'=> $state,
+                'zip' => $zip
+                
+            ]);
+            // will send user to dashboard down here once I have the document name
+            header("Location:..");
+            exit();
+        } else{
+            //sends the user back to the sign up page with an error that the email provided already has an account
+            header("location:../SignUp.html?error=UserExists");
+            exit();
+        }
     }
 }
