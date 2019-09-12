b6a8be3744cc6dcf93820ff5e23e4452b082abfd
felicia Meyer <feliciameyer1@student.purdueglobal.edu>
2019-09-12 16:00:47 -0400

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -1,21 +1,22 @@
 <?php
-    /*
-    if(isset($_POST['signUp-submit'])){
+    /* can't get this to work... will comeback later
+     *     if (isset($_POST['submit'])){
+        if(!preg_match("/^[a-zA-Z0-9]*$/",$password)){
+            echo "password too weak";
+        }
+        else{
+            echo "password is good";
+        }
+    
+}
+   
         //takes input from form into variable
         $username = $_POST['Email'];
         $password = $_POST['password'];
         $fname=$_POST['Fname'];
         $Lname=$_POST['Lname'];
-        $Address=$_POST['Address'];
-        $city = $_POST['City'];
-        $state = $_POST['State'];
-        $zip = $_POST['Zip'];
-        $role = $_POST['Role'];
-        // creates a variable for validation of password complexity against a regular expression
-        $uppercase = preg_match('@[A-Z]@', $password);
-        $lowercase = preg_match('@[a-z]@', $password);
-        $number    = preg_match('@[0-9]@', $password);
-        $specialChars = preg_match('@[^\w]@', $password);
+       
+     
         //creates a connection with the database
         $client = new MongoDB\Client("mongodb+srv://admin:admin@vetcheck-cdi31.mongodb.net/test?retryWrites=true&w=majority");
     // specifys the Mongodb collection we will be accessing
@@ -30,19 +31,11 @@
             exit();
             
         }
-        // Checks for a valid email address entered
-        else if(!filter_var($username, FILTER_VALIDATE_EMAIL)){
-            header("location:../SignUp.html?error=InvalidEmail&fname=".$fname."&lname=".$Lname."&address=".$Address."&city=".$city."&state=".$state);
-            // stops the script from running
-            exit();
+       
             
         }
         // Validates the password complexity. The password must be atleast eight charectors long with one uppercase letter, one lowercase letter, one number, and one special charector
-        else if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8){
-            header("location:../SignUp.html?error=WeakPassword&uid=".$username."&fname=".$fname."&lname=".$Lname."&address=".$Address."&city=".$city."&state=".$state);
-            // stops the script from running
-            exit();
-            
+      
         }
         else{
             $existingAccountcheck=$collection->findone(['email' => $username]);
@@ -70,23 +63,40 @@
         }
     }
     */
+$bulk = new MongoDB\Driver\BulkWrite;
 
-    $bulk = new MongoDB\Driver\BulkWrite;
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$Address=$_POST['address'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zip = $_POST['zip'];
+$role = $_POST['role'];
 
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
 
-    $user = [
+   
+   
+  
+
+   $user = [
         '_id' => new MongoDB\BSON\ObjectID,
         'firstname' => $firstname,
         'lastname' => $lastname,
         'email' => $email,
-        'password' => $password
+        'password' => $password,
+        'address' => $Address,
+        'city' => $city,
+        'state' => $state,
+        'zip' => $zip,
+        'role' => $role,
     ];
+    
+   
 
     try {
+    
         $bulk->insert($user);
         $mng = new MongoDB\Driver\Manager("mongodb+srv://admin:admin@vetcheck-cdi31.mongodb.net/test?retryWrites=true&w=majority");
         $res = $mng->executeBulkWrite('vetcheck.users', $bulk);
@@ -94,4 +104,6 @@
     } catch(MongoDB\Driver\Exception\Exception $e) {
         die('error'.$e);
     }
+
+   
 ?>
