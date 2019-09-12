2fe49da5a3e1a4a2191da2611317412f9ea53ec5
nbuikema <46764298+nbuikema@users.noreply.github.com>
2019-09-12 17:50:56 -0500

    revert signup

--- php/signup.php
+++ php/signup.php
@@ -1,16 +1,5 @@
 <?php
-  
-
-   
-   
-$bulk = new MongoDB\Driver\BulkWrite;
-
-
-
-
-
-   
-   
+    $bulk = new MongoDB\Driver\BulkWrite;
 
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
@@ -20,49 +9,40 @@ $bulk = new MongoDB\Driver\BulkWrite;
     $state = $_POST['state'];
     $zip = $_POST['zip'];
     $password = $_POST['password'];
-  
 
-
-   $user = [
+    $user = [
         '_id' => new MongoDB\BSON\ObjectID,
         'firstname' => $firstname,
         'lastname' => $lastname,
-        'email' => $email,   
+        'email' => $email,
         'address' => $address,
         'city' => $city,
         'state' => $state,
         'zip' => $zip,
-        'password' => $password
-
+        'password' => $password,
+        'pets' => []
     ];
-    
-   
 
     $userExists = false;
-
-    try {
     
+    try {
         $bulk->insert($user);
         $mng = new MongoDB\Driver\Manager("mongodb+srv://admin:admin@vetcheck-cdi31.mongodb.net/test?retryWrites=true&w=majority");
         
         $query = new MongoDB\Driver\Query([]);
         $rows = $mng->executeQuery('vetcheck.users', $query);
-
         foreach($rows as $row) {
             if($row->email == $email) {
                 $userExists = true;
             }
         }
-
         if($userExists) {
-            header("Location: ../signup.php?error=userExits");
+            header("Location: ../signup.php");
         } else {
             $res = $mng->executeBulkWrite('vetcheck.users', $bulk);
-            header("Location: ../signin.php?signupsuccess");
+            header("Location: ../signin.php");
         }
     } catch(MongoDB\Driver\Exception\Exception $e) {
         die('error'.$e);
     }
-
-   
 ?>
