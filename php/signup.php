b4a42172a32fe81423fe9df1797d20f408cc9183
nbuikema <46764298+nbuikema@users.noreply.github.com>
2019-09-12 14:58:20 -0500

    added additional fields and email validation

--- php/signup.php
+++ php/signup.php
@@ -76,6 +76,10 @@
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $email = $_POST['email'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $zip = $_POST['zip'];
     $password = $_POST['password'];
 
     $user = [
@@ -83,14 +87,34 @@
         'firstname' => $firstname,
         'lastname' => $lastname,
         'email' => $email,
+        'address' => $address,
+        'city' => $city,
+        'state' => $state,
+        'zip' => $zip,
         'password' => $password
     ];
+
+    $userExists = false;
 
     try {
         $bulk->insert($user);
         $mng = new MongoDB\Driver\Manager("mongodb+srv://admin:admin@vetcheck-cdi31.mongodb.net/test?retryWrites=true&w=majority");
-        $res = $mng->executeBulkWrite('vetcheck.users', $bulk);
-        echo 'User Created';
+        
+        $query = new MongoDB\Driver\Query([]);
+        $rows = $mng->executeQuery('vetcheck.users', $query);
+
+        foreach($rows as $row) {
+            if($row->email == $email) {
+                $userExists = true;
+            }
+        }
+
+        if($userExists) {
+            header("Location: ../signup.php");
+        } else {
+            $res = $mng->executeBulkWrite('vetcheck.users', $bulk);
+            header("Location: ../signin.php");
+        }
     } catch(MongoDB\Driver\Exception\Exception $e) {
         die('error'.$e);
     }
