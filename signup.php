3ea615c82d1d53f685507673a93035ebab0fb046
felicia Meyer <feliciameyer1@student.purdueglobal.edu>
2019-09-13 13:28:08 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -95,10 +95,10 @@
             </div>
             <div id="PasswordError">
  			 <h3>Password must contain the following:</h3>
-  			<p id="letter" class="invalid">A <b>lowercase</b> letter</p>
-  			<p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
-  			<p id="number" class="invalid">A <b>number</b></p>
-  			<p id="length" class="invalid">Minimum <b>8 characters</b></p>
+  			<p id="letter">A <b>lowercase</b> letter</p>
+  			<p id="capital">A <b>capital (uppercase)</b> letter</p>
+  			<p id="number">A <b>number</b></p>
+  			<p id="length">Minimum <b>8 characters</b></p>
 			</div>
              <div class="form-group">
                 <label for="passwordComfirm">Confirm Password</label>
@@ -120,11 +120,10 @@ var capital = document.getElementById("capital");
 var number = document.getElementById("number");
 var length = document.getElementById("length");
 var match = document.getElementById("match");
-
 // When the user clicks on the password field, show the message box
 myInput.onfocus = function() {
   document.getElementById("PasswordError").style.display = "block";
-}
+  }
 
 // When the user clicks outside of the password field, hide the message box
 myInput.onblur = function() {
@@ -136,40 +135,32 @@ myInput.onkeyup = function() {
   // Validate lowercase letters
   var lowerCaseLetters = /[a-z]/g;
   if(myInput.value.match(lowerCaseLetters)) {
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
+    letter.style.color='green';
   } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
+    letter.style.color='red';
 }
 
   // Validate capital letters
   var upperCaseLetters = /[A-Z]/g;
   if(myInput.value.match(upperCaseLetters)) {
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
+    capital.style.color='green';
   } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
+    capital.style.color='red';
   }
 
   // Validate numbers
   var numbers = /[0-9]/g;
   if(myInput.value.match(numbers)) {
-    number.classList.remove("invalid");
-    number.classList.add("valid");
+    number.style.color='green';
   } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
+    number.style.color='red';
   }
 
   // Validate length
   if(myInput.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
+    length.style.color='green';
   } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
+    length.style.color='red';
   }
 }
 //When the user clicks on the password field, show the message box
