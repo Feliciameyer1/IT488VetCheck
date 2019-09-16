b80fb1d2a66541834a83fa5356541f46fc4b5fdf
nbuikema <46764298+nbuikema@users.noreply.github.com>
2019-09-16 09:51:56 -0500

    added vet and patient to appointments

--- php/requestAppointment.php
+++ php/requestAppointment.php
@@ -1,24 +1,46 @@
 <?php
+    session_start();
+
     $bulk = new MongoDB\Driver\BulkWrite;
 
+    $vet = $_POST['vet'];
+    $patientId = $_SESSION['_id'];
     $date = $_POST['datepicker'];
     $time = $_POST['time'];
     $reasonforvisit = $_POST['reasonforvisit'];
-    $ownerId = $_SESSION['_id'];
-
-    $appointment = [
-        '_id' => new MongoDB\BSON\ObjectID,
-        'requestingUserId' => $ownerId,
-        'date' => $date,
-        'time' => $time,
-        'reasonForVisit' => $reasonforvisit,
-    ];
-
+    
     try {
-        $bulk->insert($appointment);
-
         // TODO: move connection string to the session; 2019-09-15 - Chris
         $mng = new MongoDB\Driver\Manager("mongodb+srv://admin:admin@vetcheck-cdi31.mongodb.net/test?retryWrites=true&w=majority");
+
+        $query = new MongoDB\Driver\Query([]);
+        $rows = $mng->executeQuery('vetcheck.users', $query);
+        foreach($rows as $row) {
+            if($row->practicename == $vet) {
+                $vetInfo = $row;
+                $vetInfo->password = null;
+            }
+        }
+
+        $query2 = new MongoDB\Driver\Query([]);
+        $rows = $mng->executeQuery('vetcheck.users', $query2);
+        foreach($rows as $row) {
+            if($row->_id == $patientId) {
+                $patientInfo = $row;
+                $patientInfo->password = null;
+            }
+        }
+
+        $appointment = [
+            '_id' => new MongoDB\BSON\ObjectID,
+            'vet' => $vetInfo,
+            'patient' => $patientInfo,
+            'date' => $date,
+            'time' => $time,
+            'reasonForVisit' => $reasonforvisit,
+        ];
+
+        $bulk->insert($appointment);
 
         $res = $mng->executeBulkWrite('vetcheck.appointments', $bulk);
         array_push($_SESSION['appointments'], $appointment);
@@ -26,5 +48,4 @@
     } catch(MongoDB\Driver\Exception\Exception $e) {
         die('error'.$e);
     }
-
 ?>
--- requestAppointment.php
+++ requestAppointment.php
@@ -6,6 +6,22 @@
     <h2 class="text-center">Request Appointment</h2>
     <form action="php/requestAppointment.php" method="POST">
         <div class="form-group">
+            <label for="vet">Veterinarian</label>
+            <select id="vet" name="vet" class="form-control">
+                <option value="-1">Select a vet</option>
+                <?php
+                    $mng = new MongoDB\Driver\Manager("mongodb+srv://admin:admin@vetcheck-cdi31.mongodb.net/test?retryWrites=true&w=majority");
+                    $query = new MongoDB\Driver\Query([]);
+                    $rows = $mng->executeQuery('vetcheck.users', $query);
+                    foreach($rows as $row) {
+                        if($row->practicename) {
+                            echo "<option value=\"".$row->practicename."\">".$row->practicename."</option>";
+                        }
+                    }
+                ?>
+            </select>
+        </div>
+        <div class="form-group">
             <label for="datepicker">Date</label>
             <input type="text" class="form-control" id="datepicker" name="datepicker" required>
         </div>
