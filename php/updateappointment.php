0289e650484039d452e022d14394dbe6532ad0c6
nbuikema <46764298+nbuikema@users.noreply.github.com>
2019-09-20 17:10:46 -0500

    fixed appointment shots

--- php/updateappointment.php
+++ php/updateappointment.php
@@ -12,12 +12,20 @@
     try {
         $mng = new MongoDB\Driver\Manager("mongodb+srv://admin:admin@vetcheck-cdi31.mongodb.net/test?retryWrites=true&w=majority");
 
+        $query = new MongoDB\Driver\Query([]);
+        $rows = $mng->executeQuery('vetcheck.appointments', $query);
+        foreach($rows as $row) {
+            if($row->_id == $appointmentId) {
+                $appointmentId = $row->_id;
+            }
+        }
+
         $bulk->update(array("_id" => $appointmentId), array('$push' => array("shots" => $shots)));
         $res = $mng->executeBulkWrite('vetcheck.appointments', $bulk);
+        
         $bulk2->update(array("_id" => $appointmentId), array('$push' => array("notes" => $notes)));
         $res = $mng->executeBulkWrite('vetcheck.appointments', $bulk2);
         
-        array_push($_SESSION['appointments'], $appointment);
         header("Location: ../userdashboard.php");
     } catch(MongoDB\Driver\Exception\Exception $e) {
         die('error'.$e);
