01c57a713a08dd2aa6ba45faf4cffaae8bf177db
felicia Meyer <feliciameyer1@student.purdueglobal.edu>
2019-09-21 19:18:23 -0400

    Update ApptSurveyLogic.php

--- php/ApptSurveyLogic.php
+++ php/ApptSurveyLogic.php
@@ -5,20 +5,26 @@
    
 
     $CustSat=$_POST['CustSat'];
+    $comments=$_POST['Comments'];
 
     try {
         
         $mng = new MongoDB\Driver\Manager("mongodb+srv://admin:admin@vetcheck-cdi31.mongodb.net/test?retryWrites=true&w=majority");
 
-      
+        $feedback = [
+            '_id' => new MongoDB\BSON\ObjectID,
+            'OverallSat' => $CustSat,
+            'Comments' => $comments,
+            
+        ];
 
-        $bulk->insert($CustSat);
+        $bulk->insert($feedback);
         $res = $mng->executeBulkWrite('vetcheck.ratings', $bulk);
 
        
 
         
-        header("Location: ../userdashboard.php?Survey=done");
+        header("Location: ../userdashboard.php");
     } catch(MongoDB\Driver\Exception\Exception $e) {
         die('error'.$e);
     }
