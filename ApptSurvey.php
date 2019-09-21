91d8001097102c6cb4762db80f78dd54b6e12a0f
felicia Meyer <feliciameyer1@student.purdueglobal.edu>
2019-09-21 18:49:37 -0400

    Update ApptSurvey.php

--- ApptSurvey.php
+++ ApptSurvey.php
@@ -7,21 +7,21 @@
         <form  action="php/surveysubmit.php" method="POST">
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="waittime">How satisified where you with your visit with the vet?</label>
-                    <input type="radio" class="form-control" id="waittime1" name="waittime" value="1">
-                    <label for="waittime1">Very disatisfied</label>
-                    <input type="radio" class="form-control" id="waittime2" name="waittime" value="2">
-                    <label for="waittime2">Disatisfied</label>
-                    <input type="radio" class="form-control" id="waittime3" name="waittime" value="3">
-                    <label for="waittime3">Neutral</label>
-                    <input type="radio" class="form-control" id="waittime4" name="waittime" value="4">
-                    <label for="waittime4">Satisfied</label>
-                    <input type="radio" class="form-control" id="waittime5" name="waittime" value="5">
-                    <label for="waittime5">Very Satisfied</label>
+                    <label for="CustSat">How satisified where you with your visit with the vet?</label>
+                    <input type="radio" class="form-control" id="CustSat1" name="CustSat" value="1">
+                    <label for="CustSat1">Very disatisfied</label>
+                    <input type="radio" class="form-control" id="CustSat2" name="CustSat" value="2">
+                    <label for="CustSat2">Disatisfied</label>
+                    <input type="radio" class="form-control" id="CustSat3" name="CustSat" value="3">
+                    <label for="CustSat3">Neutral</label>
+                    <input type="radio" class="form-control" id="CustSat4" name="CustSat" value="4">
+                    <label for="CustSat4">Satisfied</label>
+                    <input type="radio" class="form-control" id="CustSat5" name="CustSat" value="5">
+                    <label for="CustSat5">Very Satisfied</label>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="comments">Please tell us about your visit</label>
-                    <textarea rows="4" cols="50">
+                    <textarea class="form-control" name="Comments" rows="4" >
                     
                     </textarea>
                 </div>
