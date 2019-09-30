f526cc0f8c15988c688cc523c79113bcaa3ae1b5
Chris Darnell <csdarnell@gmail.com>
2019-09-29 21:34:16 -0400

    View Appointments - Finished; View Appointment - Finished

--- appointment.php
+++ appointment.php
@@ -9,11 +9,54 @@
             $rows = $mng->executeQuery('vetcheck.appointments', $query);
             foreach($rows as $row) {
                 if($row->_id == $_SERVER['QUERY_STRING']) {
+
+                    $userName = '';
+                    $petName = '';
+
+                    /* Try to get the user's name */
+                    try {
+                        $userFilter = [
+                            '_id' => $row->patient
+                        ];
+                    
+                        $userQuery = new MongoDB\Driver\Query($userFilter);
+                        $userRes = $mng->executeQuery('vetcheck.users', $userQuery);
+                
+                        $userRow = $userRes->toArray();
+                        if ($userRow) {
+                            $userName =  $userRow[0]->firstname.' '.$userRow[0]->lastname;
+                        }
+                    }
+                    catch (MongoDB\Driver\Exception\Exception $e) {
+                        /* if an exception occurs, show a blank user name */
+                        $userName = '';
+                    }
+
+                    /* Try to get the pet's name */
+                    try {
+                        $petFilter = [
+                            '_id' => $row->pet
+                        ];
+                    
+                        $petQuery = new MongoDB\Driver\Query($petFilter);
+                        $petRes = $mng->executeQuery('vetcheck.pets', $petQuery);
+                
+                        $petRow = $petRes->toArray();
+                        if ($petRow) {
+                            $petName = $petRow[0]->name;
+                        }
+                    }
+                    catch (MongoDB\Driver\Exception\Exception $e) {
+                        /* if an exception occurs, show a blank pet name */
+                        $petName = '';
+                    }
+
+
                     echo "Date: {$row->date}<br />
                     Time: {$row->time}<br />
                     Reason For Visit: {$row->reasonForVisit}<br />
-                    Patient: {$row->patient}<br />
-                    Pet: {$row->pet}<br />";
+                    Patient: {$userName}<br />
+                    Pet: {$petName}<br />";
                     if($_SESSION['role'] == 'Veterinarian') {
                         echo "<a href=\"./updateappointment.php?{$row->_id}\">Update Appointment</a><br /><br />";
                     } else {
--- appointments.php
+++ appointments.php
@@ -9,22 +9,66 @@
             }
         ?>
         <?php
-            foreach($_SESSION['appointments'] as $apt) {
-                $mng = new MongoDB\Driver\Manager("mongodb+srv://admin:admin@vetcheck-cdi31.mongodb.net/test?retryWrites=true&w=majority");
-                $query = new MongoDB\Driver\Query([]);
-                $rows = $mng->executeQuery('vetcheck.appointments', $query);
-                foreach($rows as $row) {
-                    if($row->_id == $apt) {
-                        echo "Date: {$row->date}<br />
-                        Time: {$row->time}<br />
-                        Reason For Visit: {$row->reasonForVisit}<br />
-                        Patient: {$row->patient}<br />
-                        Pet: {$row->pet}<br />
-                        <a href=\"./appointment.php?{$row->_id}\">View Appointment</a><br /><br />";
-                        echo"Please take the time to fill out a survey about your visit.<br>";
-                        echo "<a href='./ApptSurvey.php'>Appointment Survey</a><br /><br />";
+            $filter = [
+                'patient' => $_SESSION['_id']
+            ];
+            $options = [
+                'sort' => ['date' => 1, 'time' => 1],
+            ];
+            $mng = new MongoDB\Driver\Manager("mongodb+srv://admin:admin@vetcheck-cdi31.mongodb.net/test?retryWrites=true&w=majority");
+            $apptQuery = new MongoDB\Driver\Query( $filter, $options);
+            $apptRows = $mng->executeQuery('vetcheck.appointments', $apptQuery);
+            
+            foreach($apptRows as $row) {
+                $userName = '';
+                $petName = '';
+
+                /* Try to get the user's name */
+                try {
+                    $userFilter = [
+                        '_id' => $row->patient
+                    ];
+                
+                    $userQuery = new MongoDB\Driver\Query($userFilter);
+                    $userRes = $mng->executeQuery('vetcheck.users', $userQuery);
+            
+                    $userRow = $userRes->toArray();
+                    if ($userRow) {
+                        $userName =  $userRow[0]->firstname.' '.$userRow[0]->lastname;
                     }
                 }
+                catch (MongoDB\Driver\Exception\Exception $e) {
+                    /* if an exception occurs, show a blank user name */
+                    $userName = '';
+                }
+
+                /* Try to get the pet's name */
+                try {
+                    $petFilter = [
+                        '_id' => $row->pet
+                    ];
+                
+                    $petQuery = new MongoDB\Driver\Query($petFilter);
+                    $petRes = $mng->executeQuery('vetcheck.pets', $petQuery);
+            
+                    $petRow = $petRes->toArray();
+                    if ($petRow) {
+                        $petName = $petRow[0]->name;
+                    }
+                }
+                catch (MongoDB\Driver\Exception\Exception $e) {
+                    /* if an exception occurs, show a blank pet name */
+                    $petName = '';
+                }
+
+                echo "Date: {$row->date}<br />
+                Time: {$row->time}<br />
+                Reason For Visit: {$row->reasonForVisit}<br />
+                Patient: {$userName}<br />
+                Pet: {$petName}<br />
+                <a href=\"./appointment.php?{$row->_id}\">View Appointment</a><br /><br />";
+                echo"Please take the time to fill out a survey about your visit.<br>";
+                echo "<a href='./ApptSurvey.php'>Appointment Survey</a><br /><br />";
             }
         ?>
     </div>
