f42da68d7fd679b35649b16fa3db6408ddba724b
felicia Meyer <feliciameyer1@student.purdueglobal.edu>
2019-09-30 18:22:56 -0400

    Moved Client graph to Vet Dashboard

--- userdashboard.php
+++ userdashboard.php
@@ -1,6 +1,43 @@
 <?php
     include_once('header.php');
 ?>
+  <!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+
+      // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the bar chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Month');
+        data.addColumn('number', 'Number of new Clients');
+        data.addRows([
+          ['January', 4],
+          ['February', 3],
+          ['March',4],
+          ['April', 3],
+          ['May', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'New Client Tracker',
+                       'width':700,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
     <div class="container body">
         <div class="row">
             <?php if($_SESSION['role'] == 'Patient'): ?>
@@ -35,6 +72,8 @@
                         <p>State: <?php echo $_SESSION['state'] ?></p>
                         <p>Zip: <?php echo $_SESSION['zip'] ?></p>
                     </div>
+                     <!--Div that will hold the bar chart-->
+   					 <div id="chart_div"></div>
                 </div>
             <?php endif; ?>
         </div>
