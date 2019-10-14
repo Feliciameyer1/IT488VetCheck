990c206f9a39cfa96de1c12ea19fb42a05dff5b2
nbuikema <46764298+nbuikema@users.noreply.github.com>
2019-10-14 16:28:58 -0500

    added patient reference to survey

--- php/ApptSurveyLogic.php
+++ php/ApptSurveyLogic.php
@@ -6,16 +6,26 @@
 
     $CustSat=$_POST['CustSat'];
     $comments=$_POST['Comments'];
+    $patientId = $_SESSION['_id'];
 
     try {
         
         $mng = new MongoDB\Driver\Manager("mongodb+srv://admin:admin@vetcheck-cdi31.mongodb.net/test?retryWrites=true&w=majority");
 
+        $query2 = new MongoDB\Driver\Query([]);
+            $rows = $mng->executeQuery('vetcheck.users', $query2);
+            foreach($rows as $row) {
+                if($row->_id == $patientId) {
+                    $patientInfo = $row->_id;
+                    $patientInfo->password = null;
+                }
+            }
+
         $feedback = [
             '_id' => new MongoDB\BSON\ObjectID,
-            'OverallSat' => $CustSat,
-            'Comments' => $comments,
-            
+            'satisfaction' => $CustSat,
+            'comments' => $comments,
+            'patient' => $patientInfo,
         ];
 
         $bulk->insert($feedback);
